2441430b459b138dd8147317037bae0fc8a78b20
raquelduran <raquel.ferduran@gmail.com>
2017-02-08 23:09:27 +0100

    navbar fixed for small devices

--- src/app/topnavbar/topnavbar.component.ts
+++ src/app/topnavbar/topnavbar.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-topnavbar',
   templateUrl: './topnavbar.component.html',
   styleUrls: ['./topnavbar.component.css']
 })
 export class TopnavbarComponent implements OnInit {
+	title: string;
+	themes = [];
+	selectedTheme ='FLATLY';
+	link="https://bootswatch.com/darkly/bootstrap.min.css";
 
-  constructor() { }
-
+  constructor() { 
+  	this.title = "Spotify API Project";
+  	this.themes = [
+  	{name:'DARKLY', link: 'https://bootswatch.com/darkly/bootstrap.min.css'},
+  	{name:'FLATLY', link:'https://bootswatch.com/flatly/bootstrap.min.css'}
+  	]
+  }
   ngOnInit() {
+  }
+  onSelect(theme) {
+  this.selectedTheme = theme.name;
   }
 
 }
+
+  
