9d2e97d8e6705db8d68fccf87c92912190c4aa4e
raquelduran <raquel.ferduran@gmail.com>
2017-02-12 12:54:39 +0100

    added detail in tracks

--- src/app/tracks/tracks.component.ts
+++ src/app/tracks/tracks.component.ts
@@ -7,38 +7,38 @@ import { AJAXService } from '../ajax.service';
   styleUrls: ['./tracks.component.css']
 })
 export class TracksComponent implements OnInit {
-	topTracks: any;
+  topTracks: any;
 
-	
-	constructor(private ajax_s: AJAXService) { 
+  
+  constructor(private ajax_s: AJAXService) { 
     this.ajax_s.stablishType('track');
     this.ajax_s.peticionSearch('lagrimas%20desordenadas');
-	}
+  }
 
-  	ngOnInit() {
-  		this.ajax_s.searchTracks().subscribe(
-  			data => {
-  				this.topTracks = data;
-  				console.log(this.topTracks);
-  			})
-  	}
+    ngOnInit() {
+      this.ajax_s.searchTracks().subscribe(
+        data => {
+          this.topTracks = data;
+          console.log(this.topTracks);
+        })
+    }
 
-  	structureDuration(ms){
-  		var min;
-  		var sec;
-  		min = Math.floor(ms/60000);
-  		sec = Math.round((ms/1000)%60);
-  		if (sec<10){
-  			sec ='0'+sec;
-  		}
-  		return min+':'+sec;
+    structureDuration(ms){
+      var min;
+      var sec;
+      min = Math.floor(ms/60000);
+      sec = Math.round((ms/1000)%60);
+      if (sec<10){
+        sec ='0'+sec;
+      }
+      return min+':'+sec;
 
-  	}
-  	structureArtists(item){
-  		var Artistslist = [];
-  		for (var i = 0; i < item.length; ++i) {
-  			Artistslist.push(item[i].name);
-  		}
-  		return Artistslist.join(', ');
-  	}
+    }
+    structureArtists(item){
+      var Artistslist = [];
+      for (var i = 0; i < item.length; ++i) {
+        Artistslist.push(item[i].name);
+      }
+      return Artistslist.join(', ');
+    }
 }
